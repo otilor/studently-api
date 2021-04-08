4ebf3c442560658814cbc747b9545d59d851f708
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-04-08 13:13:13 +0100

    wip

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -18,5 +18,23 @@ class DepartmentSeeder extends Seeder
         $scienceAndScienceEducationDepartments = ['Biological Science', 'Chemical Science', 'Earth Science', 'Mathematical Science', 'Physics', 'Science Education'];
         $socialAndManagementScienceDepartments = ['Accounting', 'Business Administration', 'Economics', 'Mass Communication', 'Banking and Finance', 'Political Science and International Relations'];
         $humanitiesDepartments = ['Christian Religious Studies', 'English and Literary Studies', 'History and Diplomatic Studies', 'French'];
+
+        // process for FSSE first
+
+
+        foreach ($scienceAndScienceEducationDepartments as $department) {
+            $facultyId = Faculty::firstWhere('short_code', 'SSE')->id;
+            Department::create(['name' => $department, 'faculty_id' => $facultyId]);
+        }
+
+        foreach ($socialAndManagementScienceDepartments as $department) {
+            $facultyId = Faculty::firstWhere('short_code', 'SMS')->id;
+            Department::create(['name' => $department, 'faculty_id' => $facultyId]);
+        }
+
+        foreach ($humanitiesDepartments as $department) {
+            $facultyId = Faculty::firstWhere('short_code', 'HUM')->id;
+            Department::create(['name' => $department, 'faculty_id' => $facultyId]);
+        }
     }
 }
