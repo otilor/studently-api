2da5e34f048eff4d0aff5ec92a80b8b024e3ded1
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2021-04-08 13:03:08 +0100

    wip

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -15,14 +15,8 @@ class DepartmentSeeder extends Seeder
      */
     public function run()
     {
-        $departments = ['Biological Science','Chemical Science','Earth Science','Mathematical Science','Physics','Science Education','Accounting','Business Administration','Economics','International Relations','Political Science','Mass Communication','Christian Religious Studies','English and Literary Studies','French','History and Diplomatic Studies'];
-        $faculties = Faculty::pluck('id')->toArray();
-        $key = array_rand($faculties);
-        // dd($key);
-        foreach($departments as $department)
-        {
-            Department::create(['name' => $department,'faculty_id' => $faculties[$key]]);
-        }
-        
+        $scienceAndScienceEducationDepartments = ['Biological Science', 'Chemical Science', 'Earth Science', 'Mathematical Science', 'Physics', 'Science Education'];
+        $socialAndManagementScienceDepartments = ['Accounting', 'Business Administration', 'Economics', 'Mass Communication', 'Banking and Finance', 'Political Science and International Relations'];
+        $humanitiesDepartments = ['Christian Religious Studies', 'English and Literary Studies', 'History and Diplomatic Studies', 'French'];
     }
 }
